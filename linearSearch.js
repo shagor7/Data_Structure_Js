8ea15e0d320ec4da7b3850281f27e9f32a5b954c
shagor7 <40910111+shagor7@users.noreply.github.com>
2022-05-22 15:21:07 +0600

    Initial commit

---
+++ linearSearch.js
@@ -0,0 +1,39 @@
+// linear seacrh in js
+
+function linearaSearch(array, num) {
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] == num) {
+            return i;
+        }
+    }
+    return -1;
+}
+
+function updatedLinear(array, num) {
+    let length = array.length;
+    let left = 0;
+    let right = length - 1;
+    let position = -i;
+    while (left <= right) {
+        if (array[left] == num) {
+            position = left;
+            console.log(`${num} found at index ${position}, Attempts: ${left - 1}`);
+            break;
+        }
+        if (array[right] == num) {
+            position = right;
+            console.log(`${num} found at index ${position}, attempts: ${length - right}`);
+            break;
+        }
+    }
+}
+
+const numbers = [3, 14, 56, 34, 7, 89, 26]
+
+const result = linearaSearch(numbers, 56);
+
+if (result == -1) {
+    console.log('element not found');
+} else {
+    console.log(`element found at index ${result}`);
+}
